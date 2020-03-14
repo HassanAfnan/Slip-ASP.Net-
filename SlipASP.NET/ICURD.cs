75b99c041f41386d990819398dbacf1ad8ccb0c0
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-03-14 16:56:51 +0500

    Add files via upload

---
+++ SlipASP.NET/ICURD.cs
@@ -0,0 +1,15 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlipASP.NET
+{
+    interface ICURD<T>
+    {
+        StringReader Recipt(T a);
+    }
+}
